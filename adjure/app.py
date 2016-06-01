109833b8bfd8dccc63dc7bed6a7a61fe6d160507
Ben Picolo <be.picolo@gmail.com>
2016-06-01 08:53:47 -0700

    Use better docker practices for logging, db host

--- adjure/app.py
+++ adjure/app.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import argparse
 import logging
+import os
+import sys
 
 import staticconf
 from flask import Flask
@@ -10,17 +12,16 @@ from adjure.models.base import bind_database_engine
 from adjure.routes.auth import auth_page
 
 
+DB_HOST_ENV_VAR = 'ADJURE_DB_HOST'
+
+
 def register_app_config(config_path):
     staticconf.YamlConfiguration(config_path, namespace='adjure')
     return staticconf.NamespaceReaders('adjure')
 
 
 def setup_logging(app, config):
-    log_path = config.read('logging.log_path', default=None)
-    if not log_path:
-        return
-
-    handler = logging.FileHandler(log_path)
+    handler = logging.StreamHandler(sys.stdout)
     handler.setFormatter(LogstashFormatterV1())
     app.logger.addHandler(handler)
 
@@ -36,7 +37,12 @@ parser.add_argument('--config', dest='config_path', default='config.example.yaml
 
 args = parser.parse_args()
 config = register_app_config(args.config_path)
-bind_database_engine(config)
 
+if not os.environ.get(DB_HOST_ENV_VAR, None):
+    raise ValueError(
+        '{} is required for Adjure to connect to a database.'.format(DB_HOST_ENV_VAR)
+    )
+
+bind_database_engine(os.environ[DB_HOST_ENV_VAR])
 application = build_app()
 setup_logging(application, config)
